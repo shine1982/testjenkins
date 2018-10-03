7dd58b04c12c99de1cd49b5f2552035857f75be1
Feng QIN <qinfeng0152@gmail.com>
2018-10-04 01:23:37 +0200

    init commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * Created by fengqin on 04/10/2018.
+ */
+public class Hello {
+
+    public String say(String name){
+        if ("feng".endsWith(name)){
+            return "hello Feng";
+        }
+        return "hello world";
+    }
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by fengqin on 04/10/2018.
+ */
+public class HelloTest {
+
+    @org.junit.Test
+    public void testSay() throws Exception {
+
+
+
+    }
+}
